e7d932414669d822bf77075de0b948633b6ceafe
Rahul Umak <rahulumak22@gmail.com>
2021-02-12 02:06:21 +0530

    5 Days Forecast Added.

---
+++ app/src/main/java/com/rahulumak/mobiquity/mobiquityassignment/ui/city/DayForecast.kt
@@ -0,0 +1,9 @@
+package com.rahulumak.mobiquity.mobiquityassignment.ui.city
+
+import com.rahulumak.mobiquity.mobiquityassignment.model.ForecastModel
+
+class DayForecast(
+    var dateText: String,
+    var dayText: String,
+    var list: ArrayList<ForecastModel.ForecastList>
+)
